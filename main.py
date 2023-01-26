e3f6fda23f817d29d88886d5b4c88daf29502054
Pierre Fils <epierrefils@gmail.com>
2023-01-26 15:01:32 -0500

    Bank Management System with Sql connectivity

--- main.py
+++ main.py
@@ -1,16 +1,113 @@
-# This is a sample Python script.
+import mysql.connector
+mydb = mysql.connector.connect(host='localhost',user='root',password='rootpass',database ='BANK_MANAGEMENT')
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def OpenAcc():
+    n = input("Enter the Name :")
+    ac= input("Enter the Account No :")
+    db = input("Enter the Date of Birth :")
+    add = input("Enter the Address :")
+    cn = input("Enter the Contact Number :")
+    ob= int(input("Enter the Opening Balance :"))
+    data1 = (n,ac,db,add,cn,ob)
+    data2= (n,ac,ob)
+    sql1=('insert into account values (%s,%s,%s,%s,%s,%s)')
+    sql2= ('insert into amount values (%s,%s,%s)')
+    x=mydb.cursor()
+    x.execute(sql1,data1)
+    x.execute(sql2,data2)
+    mydb.commit()
+    print("Data entered Successfully..")
+    main()
+
+def DepoAmo():
+    amount = input("Enter the amount you want to deposit:")
+    ac= input("Enter the Account No :")
+    a= 'select balance from amount where AccNo=%s'
+    data =(ac,)
+    x=mydb.cursor()
+    x.execute(a,data)
+    result = x.fetchone()
+    t=result[0]+amount
+    sql=('update amuont set balance where AccNo = %s')
+    d=(t,ac)
+    x.execute(sql,d)
+    mydb.commit()
+    main()
+
+def WithdrawAmo():
+    amount = input("Enter the amount you want to withdraw:")
+    ac = input("Enter the Account No :")
+    a = 'select balance from amount where AccNo=%s'
+    data = (ac,)
+    x = mydb.cursor()
+    x.execute(a, data)
+    result = x.fetchone()
+    t = result[0] - amount
+    sql = ('update amuont set balance where AccNo = %s')
+    d = (t, ac)
+    x.execute(sql, d)
+    mydb.commit()
+    main()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def BalEnq ():
+    ac = input("Enter the Account No :")
+    a = 'select * from amount where AccNo=%s'
+    data = (ac,)
+    x = mydb.cursor()
+    x.execute(a, data)
+    result = x.fetchone()
+    print("Balance for account :",ac,"is",result[-1])
+    main()
+
+def DisDetails():
+    ac = input("Enter the Account No :")
+    a = 'select * from amount where AccNo=%s'
+    data = (ac,)
+    x = mydb.cursor()
+    x.execute(a, data)
+    result = x.fetchone()
+    for i in result:
+        print(i)
+    main()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def closeAcc() :
+    ac = input("Enter the Account No :")
+    sql1= 'delete from account where AccNo=%s'
+    sql2 = 'delete from account where AccNo=%s'
+    data = (ac,)
+    x = mydb.cursor()
+    x.execute(sql1, data)
+    x.execute(sql2, data)
+    mydb.commit()
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def main():
+    print('''
+            1.OPERN ACCOUNT
+            2. DEPOSITE AMOUNT
+            3.WITHDRAW AMOUNT
+            4.BALANCE ENQUIRY
+            5.DISPLAY CUSTOMER DETAILS
+            6.CLOSE AN ACCOUNT
+            ''');
+
+    choice = input("Enter the task you want to perform :")
+    if(choice =='1'):
+        OpenAcc();
+    elif(choice =='2'):
+        DepoAmo();
+    elif (choice == '3'):
+        WithdrawAmo();
+    elif(choice =='4'):
+        BalEnq();
+    elif(choice =='5'):
+        DisDetails();
+    elif (choice == '6'):
+        closeAcc();
+    else :
+        print("Invalid Choice")
+        main()
+main()
